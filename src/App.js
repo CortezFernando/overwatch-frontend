97ff5dab7de629b0a02e9e2acf9b10ac1a4a5aaa
Fernando Cortez <cortez.fernando11216@gmail.com>
2023-05-18 22:57:41 -0400

    created components and pages and paths

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//pages
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import NewOverwatchHero from "./Pages/NewOverwatchHero";
+import EditOverwatchHero from "./Pages/EditOverwatchHero";
+import OverwatchHeroById from "../Pages/OverwatchHeroById";
+import FourOFour from "./Pages/FourOFour";
+
+
+
+//Components
+import NavBar from "./Components/Nav.Bar";
+import About from "./Components/About.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/OverwatchHero" element={<Index />} />
+            <Route exact path="/OverwatchHero/:overwatchId" element={<OverwatchHeroById/>} />
+            <Route path="/OverwatchHero/new" element={<NewOverwatchHero />} />
+            <Route path="/OverwatchHero/:overwatchId/edit" element={<EditOverwatchHero />} />
+             <Route path="*" element={<FourOFour />} />  
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
